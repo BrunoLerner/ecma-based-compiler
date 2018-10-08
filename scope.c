25413f6ccb5c677d1586efab485b5d8429d30556
BrunoLerner <bru.lerner@gmail.com>
2018-10-08 19:28:25 -0300

    add scope.c

---
+++ scope.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "scope.h"
+
+//TODO what is the max nest level
+#define MAX_NEST_LEVEL 10
+
+pobject SymbolTable[MAX_NEST_LEVEL];
+pobject SymbolTableLast[MAX_NEST_LEVEL];
+int nCurrentLevel = -1;
+
+int NewBlock(void)
+{
+    SymbolTable[++nCurrentLevel] = NULL;
+    SymbolTableLast[nCurrentLevel] = NULL;
+    return nCurrentLevel;
+}
+
+int EndBlock(void)
+{
+    return --nCurrentLevel;
+}
